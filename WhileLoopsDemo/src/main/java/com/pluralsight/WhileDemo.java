d48be5d3f59bfd8dcf83c5bf332e86c6d39659cd
Bobby Johnson <bobbyj122703@gmail.com>
2025-04-21 11:31:10 -0500

    While Loops

---
+++ WhileLoopsDemo/src/main/java/com/pluralsight/WhileDemo.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class WhileDemo {
+    public static void main(String[] args) {
+        System.out.println("Test!");
+
+        int i = 10;
+
+         while(i > 0){
+            System.out.println(i);
+
+            i--;
+    }
+
+        System.out.println("Blast off!");
+
+        for(int i = 10; i >0; i--){
+        }
+        System.out.println("Blast off!");
+
+    }
+}
